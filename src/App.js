c0ccb53517ae726211d9b8f9034b344d7e6fda63
Kristine Valdez <tinz_18@yahoo.com.ph>
2021-07-30 10:46:11 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+import { useState } from 'react';
+import Search from './components/Search';
+import Header from './components/Header';
+
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "26/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "27/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "28/07/2021",
+    },
+
+    
+],
+);
+
+
+const [searchText, setSearchText] = useState('');
+//const [searchDate, setSearchDate] = useState('');
+
+const searchDate = (date) => {
+  //alert(date !=='')
+   if(date !== ''){
+    const newDate = notes.filter((note)=>
+    note.date.includes(date.toLocaleDateString()))
+    setNotes(newDate);
+   }else{
+    const newDate = notes.filter((note)=>
+    note.date!=='')
+    setNotes(newDate);
+   }
+ 
+}
+
+
+const addNote = (text) => {
+  const date = new Date();
+  const newNote = {
+    id: nanoid(),
+    text: text,
+    date: date.toLocaleDateString(),
+  }
+  const newNotes = [...notes, newNote];
+  setNotes(newNotes);
+};
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note)=> note.id !== id);
+  setNotes(newNotes);
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      
+      <Header handlerSearchDate={searchDate}/>
+      <Search handleSearchNote={setSearchText}/>
+       
+      <NotesList notes={notes.filter((note)=>
+          note.text.toLowerCase().includes(searchText))}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+      />
     </div>
   );
 }
